960ee3692e00e792493c8e6b67b83aa4b68da07e
Bilguun-Erdene Munkhbat <bilguuneerw@gmail.com>
2026-09-12 09:29:24 +0800

    Lab2: K6 load testing setup

---
+++ test-basic.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 1,
+  duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<332'],
+  },
+};
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
