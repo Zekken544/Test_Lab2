2562758e3e58a87f3586b1f3862602ad50607f26
Bilguun-Erdene Munkhbat <bilguuneerw@gmail.com>
2026-09-12 09:57:42 +0800

    Added comments on test-basic.js

--- test-basic.js
+++ test-basic.js
@@ -2,10 +2,10 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 1,
-  duration: '30s',
+  vus: 1,  //<---- VU uurcilj turshih bol ene
+  duration: '30s', 
   thresholds: {
-    http_req_duration: ['p(95)<332'],
+    http_req_duration: ['p(95)<332'], //<---- Threshold uurcluh bol ene
   },
 };
 
